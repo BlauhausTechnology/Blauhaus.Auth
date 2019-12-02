b852907e03c343c750938fc3f38ba914a403a885
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-12-02 16:17:32 +0200

    oops transient

--- src/Blauhaus.Auth.Abstractions/_Ioc/IocRegistration.cs
+++ src/Blauhaus.Auth.Abstractions/_Ioc/IocRegistration.cs
@@ -7,13 +7,13 @@ namespace Blauhaus.Auth.Abstractions._Ioc
     {
         public static IIocService RegisterAccessToken(this IIocService iocService)
         {
-            iocService.RegisterImplementation<IAuthenticatedAccessToken, AuthenticatedAccessToken>();
+            iocService.RegisterImplementation<IAuthenticatedAccessToken, AuthenticatedAccessToken>(IocLifetime.Singleton);
             return iocService;
         }
         
         public static IIocService RegisterAccessToken<TAccessToken>(this IIocService iocService) where TAccessToken : AuthenticatedAccessToken
         {
-            iocService.RegisterImplementation<IAuthenticatedAccessToken, TAccessToken>();
+            iocService.RegisterImplementation<IAuthenticatedAccessToken, TAccessToken>(IocLifetime.Singleton);
             return iocService;
         }
     }
