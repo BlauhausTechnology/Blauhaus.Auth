225cc0a56d0b652a101a79e7aa3764e91fcff0c6
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2019-12-10 17:35:19 +0200

    no message

--- src/Blauhaus.Auth.Abstractions/ClientAuthenticationHandlers/IAuthenticatedAccessToken.cs
+++ src/Blauhaus.Auth.Abstractions/ClientAuthenticationHandlers/IAuthenticatedAccessToken.cs
@@ -1,11 +1,16 @@
-﻿namespace Blauhaus.Auth.Abstractions.ClientAuthenticationHandlers
+﻿using System.Collections.Generic;
+
+namespace Blauhaus.Auth.Abstractions.ClientAuthenticationHandlers
 {
     public interface IAuthenticatedAccessToken
     {
         void SetAccessToken(string scheme, string authenticatedAccessToken);
-        void ClearAccessToken();
+        void SetHeader(string key, string value);
 
         string Scheme { get; }
         string Token { get; }
+        IReadOnlyDictionary<string, string> AdditionalHeaders { get; }
+        void Clear();
+
     }
 }
