6b7060aa0a4e4667a6767b11c26adf5f009b5d20
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-03-14 08:35:57 +0200

    no message

---
+++ src/Blauhaus.Auth.Client.Azure/_Ioc/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Blauhaus.Analytics.Console._Ioc;
+using Blauhaus.Auth.Abstractions._Ioc;
+using Blauhaus.Auth.Abstractions.Services;
+using Blauhaus.Auth.Client.Azure.Config;
+using Blauhaus.Auth.Client.Azure.MsalProxy;
+using Blauhaus.Auth.Client.Azure.Service;
+using Blauhaus.Common.Time._Ioc;
+using Blauhaus.Ioc.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Blauhaus.Auth.Client.Azure._Ioc
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection RegisterAzureAuthenticationClient<TConfig>(this IServiceCollection services) 
+            where TConfig : class, IAzureActiveDirectoryClientConfig
+        {
+            Register<TConfig>(services);
+            services.RegisterAccessToken();
+            return services;
+        }
+
+        private static void Register<TConfig>(IServiceCollection services) where TConfig : class, IAzureActiveDirectoryClientConfig
+        {
+            services.AddScoped<IAzureActiveDirectoryClientConfig, TConfig>();
+            services.AddScoped<IAuthenticationClientService, AzureAuthenticationClientService>();
+            services.AddScoped<IMsalClientProxy, MsalClientProxy>();
+            services.RegisterConsoleLoggerClientService();
+            services.RegisterTimeService();
+
+        }
+    }
+}
