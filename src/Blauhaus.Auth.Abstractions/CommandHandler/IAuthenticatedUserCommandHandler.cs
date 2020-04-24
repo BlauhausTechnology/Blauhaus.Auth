afb7315b877d044c090a0c7099bf50ea6be24ff2
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-04-24 11:45:58 +0200

    no message

--- src/Blauhaus.Auth.Abstractions/CommandHandler/IAuthenticatedUserCommandHandler.cs
+++ src/Blauhaus.Auth.Abstractions/CommandHandler/IAuthenticatedUserCommandHandler.cs
@@ -1,11 +1,10 @@
 ﻿using Blauhaus.Auth.Abstractions.User;
 using Blauhaus.Common.Domain.CommandHandlers;
-using Blauhaus.Common.Domain.CommandHandlers.Server;
 
 namespace Blauhaus.Auth.Abstractions.CommandHandler
 {
     public interface IAuthenticatedUserCommandHandler<TPayload, TCommand> 
-        : ICommandServerHandler<TPayload, TCommand, IAuthenticatedUser>
+        : IAuthenticatedCommandHandler<TPayload, TCommand, IAuthenticatedUser>
         where TCommand : notnull
     {
         
--- src/Blauhaus.Auth.Server.Azure/_Ioc/ServiceCollectionExtensions.cs
+++ src/Blauhaus.Auth.Server.Azure/_Ioc/ServiceCollectionExtensions.cs
@@ -6,7 +6,7 @@ using Blauhaus.Auth.Abstractions.User;
 using Blauhaus.Auth.Server.Azure.AdalProxy;
 using Blauhaus.Auth.Server.Azure.Config;
 using Blauhaus.Auth.Server.Azure.Service;
-using Blauhaus.Common.Domain.CommandHandlers.Server;
+using Blauhaus.Common.Domain.CommandHandlers;
 using Blauhaus.HttpClientService._Ioc;
 using Blauhaus.Ioc.Abstractions;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,7 +31,7 @@ namespace Blauhaus.Auth.Server.Azure._Ioc
         public static IServiceCollection AddAuthenticatedUserCommandHandler<TPayload, TCommand, TCommandHandler>(this IServiceCollection services) 
             where TCommandHandler : class, IAuthenticatedUserCommandHandler<TPayload, TCommand>
         {
-            services.AddScoped<ICommandServerHandler<TPayload, TCommand, IAuthenticatedUser>, TCommandHandler>();
+            services.AddScoped<IAuthenticatedCommandHandler<TPayload, TCommand, IAuthenticatedUser>, TCommandHandler>();
             services.AddScoped<IAuthenticatedUserCommandHandler<TPayload, TCommand>, TCommandHandler>();
             return services;
         }
