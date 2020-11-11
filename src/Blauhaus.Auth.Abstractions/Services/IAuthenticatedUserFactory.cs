5f9305a485981c0e5d5e69be5f8d24c811df6d3e
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-11-11 09:29:19 +0200

    no message

--- src/Blauhaus.Auth.Abstractions/Services/IAuthenticatedUserFactory.cs
+++ src/Blauhaus.Auth.Abstractions/Services/IAuthenticatedUserFactory.cs
@@ -1,7 +1,6 @@
 ﻿using System.Security.Claims;
 using Blauhaus.Auth.Abstractions.User;
 using Blauhaus.Responses;
-using CSharpFunctionalExtensions;
 
 namespace Blauhaus.Auth.Abstractions.Services
 {
