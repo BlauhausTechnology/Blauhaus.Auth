68fed79b4d92005fc86f1f41ae8be4382dd26ac6
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-04-21 15:08:40 +0200

    no message

--- src/Blauhaus.Auth.TestHelpers/MockBuilders/AuthenticatedUserMockBuilder.cs
+++ src/Blauhaus.Auth.TestHelpers/MockBuilders/AuthenticatedUserMockBuilder.cs
@@ -7,14 +7,21 @@ namespace Blauhaus.Auth.TestHelpers.MockBuilders
 {
     public class AuthenticatedUserMockBuilder : BaseMockBuilder<AuthenticatedUserMockBuilder, IAuthenticatedUser>
     {
+
+        private readonly List<UserClaim> _claims = new();
+
         public AuthenticatedUserMockBuilder(Guid? userId = null)
         {
             var id = userId ?? Guid.NewGuid();
             With(x => x.EmailAddress, Guid.NewGuid() + "@freever.com");
             With(x => x.UserId, id);
-            With(x => x.Claims, new List<UserClaim>());
+            With(x => x.Claims, _claims);
         }
-
+        public AuthenticatedUserMockBuilder With_EmailAddress(string emailAddress)
+        {
+            With(x => x.EmailAddress, emailAddress);
+            return this;
+        }
         public AuthenticatedUserMockBuilder With_UserId(Guid id)
         {
             With(x => x.UserId, id);
@@ -22,9 +29,15 @@ namespace Blauhaus.Auth.TestHelpers.MockBuilders
         }
         public AuthenticatedUserMockBuilder With_Claim(UserClaim claim)
         {
+            var val = claim.Value;
+
             Mock.Setup(x => x.HasClaim(claim.Name)).Returns(true);
             Mock.Setup(x => x.HasClaimValue(claim.Name, claim.Value)).Returns(true);
-            With(x => x.Claims, new List<UserClaim>{claim});
+            Mock.Setup(x => x.TryGetClaimValue(claim.Name, out val)).Returns(true);
+            
+            _claims.Add(claim);
+            With(x => x.Claims, _claims);
+            
             return this;
         }
     }
